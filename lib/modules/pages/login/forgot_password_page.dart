f17def8d03187d9a995a14dd35a3566ad52ec7e7
Nicolas Escorcia <nicolasescorcia@Nicolass-MacBook-Pro.local>
2022-12-23 11:42:55 -0500

    Forgot password reviewing details

--- lib/modules/pages/login/forgot_password_page.dart
+++ lib/modules/pages/login/forgot_password_page.dart
@@ -3,6 +3,7 @@ import 'package:swagapp/generated/l10n.dart';
 import 'package:swagapp/modules/common/ui/primary_button.dart';
 import 'package:swagapp/modules/common/utils/palette.dart';
 
+import '../../common/ui/custom_app_bar.dart';
 import '../../common/ui/custom_text_form_field.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
@@ -42,8 +43,10 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       resizeToAvoidBottomInset: true,
       backgroundColor: Colors.transparent,
+      appBar: CustomAppBar(),
       body: GestureDetector(
           behavior: HitTestBehavior.translucent,
           onTap: () {
@@ -62,104 +65,114 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                   ),
                   child: null),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: SafeArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Image.asset(
-                      'assets/images/logo.png',
-                      width: 125,
-                      height: 51,
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                        _codeView
-                            ? "Please check your email and enter your six digit code below."
-                            : "Enter your email to reset your password.",
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineLarge!
-                            .copyWith(
-                                color: Palette.current.primaryWhiteSmoke,
-                                fontSize: 18)),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    CustomTextFormField(
-                        autofocus: false,
-                        labelText: _codeView ? "Code" : "Email",
-                        focusNode: _codeView ? _codeNode : _emailNode,
-                        accountController:
-                            _codeView ? _codeController : _emailController,
-                        onChanged: (value) {},
-                        inputType: TextInputType.text),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    PrimaryButton(
-                      title: "RESET PASSWORD",
-                      onPressed: () {
-                        setState(() {
-                          _codeView = true;
-                        });
-                      },
-                      type: PrimaryButtonType.green,
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Visibility(
-                      visible: _codeView,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              //Test variable to see dynamic behavior
-                              setState(() {
-                                _codeView = false;
-                              });
-                            },
-                            child: Text("Didn’t get an email?",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headlineLarge!
-                                    .copyWith(
-                                        color: Palette.current.primaryNeonGreen,
-                                        fontSize: 18)),
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              //Test variable to see dynamic behavior
-                              setState(() {
-                                _codeView = false;
-                              });
-                            },
-                            child: Text("Resend Email.",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headlineLarge!
-                                    .copyWith(
-                                        fontWeight: FontWeight.bold,
-                                        color: Palette.current.primaryNeonGreen,
-                                        fontSize: 18)),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+            LayoutBuilder(builder: (context, viewportConstraints) {
+              return SingleChildScrollView(
+                  child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
                 ),
-              ),
-            ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SafeArea(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Image.asset(
+                          'assets/images/logo.png',
+                          width: 125,
+                          height: 51,
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                            _codeView
+                                ? "Please check your email and enter your six digit code below."
+                                : "Enter your email to reset your password.",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineLarge!
+                                .copyWith(
+                                    color: Palette.current.primaryWhiteSmoke,
+                                    fontSize: 16)),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        CustomTextFormField(
+                            autofocus: false,
+                            labelText: _codeView ? "Code" : "Email",
+                            focusNode: _codeView ? _codeNode : _emailNode,
+                            accountController:
+                                _codeView ? _codeController : _emailController,
+                            onChanged: (value) {},
+                            inputType: TextInputType.text),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        PrimaryButton(
+                          title: "RESET PASSWORD",
+                          onPressed: () {
+                            setState(() {
+                              _codeView = true;
+                            });
+                          },
+                          type: PrimaryButtonType.green,
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Visibility(
+                          visible: _codeView,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  //Test variable to see dynamic behavior
+                                  setState(() {
+                                    _codeView = false;
+                                  });
+                                },
+                                child: Text("Didn’t get an email?",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineLarge!
+                                        .copyWith(
+                                            color: Palette
+                                                .current.primaryNeonGreen,
+                                            fontSize: 16)),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  //Test variable to see dynamic behavior
+                                  setState(() {
+                                    _codeView = false;
+                                  });
+                                },
+                                child: Text("Resend Email.",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineLarge!
+                                        .copyWith(
+                                            fontWeight: FontWeight.bold,
+                                            color: Palette
+                                                .current.primaryNeonGreen,
+                                            fontSize: 16)),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ));
+            })
           ])),
     );
   }
