52b7dabd62777d621fbc903c8bfb4bfe132e3d87
Nicolas Escorcia <nicolasescorcia@Nicolass-MacBook-Pro.local>
2023-02-14 20:16:57 -0500

    ASA-557 List item for sale - Field validation issue

--- lib/modules/pages/add/collection/list_for_Sale_page.dart
+++ lib/modules/pages/add/collection/list_for_Sale_page.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:simple_rich_text/simple_rich_text.dart';
 
 import '../../../../generated/l10n.dart';
@@ -12,7 +12,6 @@ import '../../../common/ui/custom_text_form_field.dart';
 import '../../../common/ui/multi_image_slide.dart';
 import '../../../common/ui/popup_image_guidelines.dart';
 
-import '../../../common/ui/popup_item_to_collection_error.dart';
 import '../../../common/ui/primary_button.dart';
 import '../../../common/utils/custom_route_animations.dart';
 import '../../../common/utils/palette.dart';
@@ -44,8 +43,6 @@ class _ListForSalePageState extends State<ListForSalePage> {
   final ImagePicker imagePicker = ImagePicker();
   List<XFile> imageFileList = [];
 
-  DateTime? _defaultDateTime;
-
   final FocusNode _listPriceItemNode = FocusNode();
   final _listPriceItemController = TextEditingController();
   Color _listPriceItemBorder = Palette.current.primaryWhiteSmoke;
@@ -58,8 +55,15 @@ class _ListForSalePageState extends State<ListForSalePage> {
 
   String? listPriceItemErrorText;
   String? conditionErrorText;
+  String? descriptionErrorText;
 
   String _defaultCondition = 'Condition';
+
+  String _price = '';
+
+  bool _decimalFlag = false;
+
+  final formater = NumberFormat("##0.00");
 
   var Conditions = [
     'Condition',
@@ -290,7 +294,7 @@ class _ListForSalePageState extends State<ListForSalePage> {
                                   Align(
                                     alignment: Alignment.centerLeft,
                                     child: Text(
-                                        "${S.of(context).for_sale}: \$ ${_listPriceItemController.text.toString()}",
+                                        "${S.of(context).for_sale}: \$$_price",
                                         textAlign: TextAlign.left,
                                         overflow: TextOverflow.fade,
                                         style: Theme.of(context)
@@ -316,26 +320,63 @@ class _ListForSalePageState extends State<ListForSalePage> {
                                 height: 5,
                               ),
                               CustomTextFormField(
-                                  suffix: Image.asset(
-                                    'assets/images/trending-up.png',
-                                    width: 20,
-                                    height: 20,
-                                    scale: 3,
-                                    color: Palette.current.blackSmoke,
-                                  ),
-                                  inputFormatters: [
-                                    FilteringTextInputFormatter.allow(
-                                        RegExp('[0-9.,]')),
-                                  ],
-                                  borderColor: _listPriceItemBorder,
-                                  autofocus: false,
-                                  errorText: listPriceItemErrorText,
-                                  labelText: S.of(context).list_price_input,
-                                  focusNode: _listPriceItemNode,
-                                  controller: _listPriceItemController,
-                                  inputType:
-                                      const TextInputType.numberWithOptions(
-                                          decimal: true, signed: true)),
+                                suffix: Image.asset(
+                                  'assets/images/trending-up.png',
+                                  width: 20,
+                                  height: 20,
+                                  scale: 3,
+                                  color: Palette.current.blackSmoke,
+                                ),
+                                maxLength: _decimalFlag ? 6 : 4,
+                                onChanged: (priceInput) {
+                                  //TODO Start Format validations for prices
+                                  setState(() {
+                                    if (priceInput.isNotEmpty &&
+                                        (!_listPriceItemController.text
+                                            .toString()
+                                            .contains(','))) {
+                                      _decimalFlag = false;
+                                      _price = formater
+                                          .format(int.parse(priceInput));
+                                    } else {
+                                      setState(() {
+                                        _decimalFlag = true;
+                                        _price = _listPriceItemController.text
+                                            .toString();
+                                      });
+                                    }
+                                  });
+                                  if (_listPriceItemController.text
+                                              .toString()
+                                              .length >
+                                          3 &&
+                                      _listPriceItemController.text
+                                              .toString()[3] !=
+                                          ',') {
+                                    setState(() {
+                                      _listPriceItemController.text =
+                                          _listPriceItemController.text
+                                              .toString()
+                                              .substring(0, 3);
+                                      _price = formater.format(int.parse(
+                                          _listPriceItemController.text
+                                              .toString()));
+                                    });
+                                  }
+                                  //TODO End Format validations for prices
+                                },
+                                borderColor: _listPriceItemBorder,
+                                autofocus: false,
+                                errorText: listPriceItemErrorText,
+                                labelText: S.of(context).list_price_input,
+                                focusNode: _listPriceItemNode,
+                                controller: _listPriceItemController,
+                                inputType:
+                                    const TextInputType.numberWithOptions(
+                                  decimal: true,
+                                  signed: false,
+                                ),
+                              ),
                               const SizedBox(
                                 height: 20,
                               ),
@@ -363,7 +404,9 @@ class _ListForSalePageState extends State<ListForSalePage> {
                                 decoration: BoxDecoration(
                                     border: Border.all(
                                         width: 0.5,
-                                        color: Palette.current.grey)),
+                                        color: descriptionErrorText != null
+                                            ? Palette.current.primaryNeonPink
+                                            : Palette.current.grey)),
                                 child: TextField(
                                   controller: _listDescriptionItemController,
                                   keyboardType: TextInputType.text,
@@ -391,6 +434,20 @@ class _ListForSalePageState extends State<ListForSalePage> {
                                   ),
                                 ),
                               ),
+                              const SizedBox(
+                                height: 4,
+                              ),
+                              descriptionErrorText != null
+                                  ? Text(
+                                      descriptionErrorText ?? '',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall!
+                                          .copyWith(
+                                              color: Palette
+                                                  .current.primaryNeonPink),
+                                    )
+                                  : Container(),
                               const SizedBox(
                                 height: 20,
                               ),
@@ -435,12 +492,17 @@ class _ListForSalePageState extends State<ListForSalePage> {
       conditionErrorText = _defaultCondition != 'Condition'
           ? null
           : S.of(context).required_field;
+
+      descriptionErrorText = _listDescriptionItemController.text.isNotEmpty
+          ? null
+          : S.of(context).required_field;
     });
   }
 
   bool areFieldsValid() {
     return _listPriceItemController.text.isNotEmpty &&
-        _defaultCondition != 'Condition';
+        _defaultCondition != 'Condition' &&
+        _listDescriptionItemController.text.isNotEmpty;
   }
 
   Future<void> selectImages() async {
