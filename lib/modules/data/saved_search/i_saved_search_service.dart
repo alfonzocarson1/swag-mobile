68787c3133ec42037b3d7a150c64a73cfbfc9c44
juanfactor88 <juan.perez@blankfactor.com>
2023-04-05 09:06:12 -0500

    ASA-1339 <api client created for saved searches endpoint>

---
+++ lib/modules/data/saved_search/i_saved_search_service.dart
@@ -0,0 +1,9 @@
+
+import 'package:swagapp/modules/models/saved_searches/saved_search.dart';
+
+abstract class ISavedSearchService {
+
+  Future<List<SavedSearch>> getSavedSearchs();
+  Future saveSearch(SavedSearch search);
+
+}
---
+++ lib/modules/data/saved_search/saved_search_service.dart
@@ -0,0 +1,63 @@
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:swagapp/modules/data/saved_search/i_saved_search_service.dart';
+import 'package:swagapp/modules/models/saved_searches/saved_search.dart';
+
+import '../../api/api.dart';
+import '../../api/api_service.dart';
+import '../../common/utils/utils.dart';
+import '../../di/injector.dart';
+import '../../models/saved_searches/saved_search_result.dart';
+import '../secure_storage/storage_repository_service.dart';
+import '../shared_preferences/shared_preferences_service.dart';
+
+class SavedSearchService implements ISavedSearchService {
+  SavedSearchService(this.apiService);
+
+  final APIService apiService;  
+  
+  @override
+  Future<List<SavedSearch>> getSavedSearchs() async {
+    
+    bool isLogged = getIt<PreferenceRepositoryService>().isLogged();
+    String? token = await getIt<StorageRepositoryService>().getToken();
+    //String token2 = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzYTY3Yzc3NC02YmU3LTRlNmItODg2ZS1iZTIyYjBkYjZhMzkiLCJpYXQiOjE2Nzk5NDM3NTQsImV4cCI6MTY4MDk2NDE0Nn0.7ghnHGLRPVnZ5rm-Ih_07ULYTgQOZxFbnEzk1-ePxPE";
+    bool isAuthenticatedUser = isLogged && isTokenValid(token);
+
+    final response = await apiService.getEndpointData(
+      endpoint: Endpoint.savedSearches,
+      method: RequestMethod.get,
+      jsonKey: "savedSearchResult",
+      needBearer: isAuthenticatedUser,
+      fromJson: (json) => SavedSearchResult.fromJson(json),
+    );
+    List<SavedSearch> lista= response.savedSearchResult;
+
+    return lista;    
+  }
+  
+  @override
+  Future saveSearch(SavedSearch search) async {
+    bool isLogged = getIt<PreferenceRepositoryService>().isLogged();
+    String? token = await getIt<StorageRepositoryService>().getToken();
+    bool isAuthenticatedUser = isLogged && isTokenValid(token);
+
+
+     try {
+       final response = await apiService.getEndpointData(
+      endpoint: Endpoint.savedSearches,
+      method: RequestMethod.post,
+      body: search.toJson(),
+      needBearer: isAuthenticatedUser,
+    );
+     }
+     catch(e){
+      debugPrint(e.toString());
+     }   
+
+
+  }
+  
+}
