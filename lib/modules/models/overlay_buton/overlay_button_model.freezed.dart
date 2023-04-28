5f27d89212e44a68dd7387673dd102ca3c6a539b
juanfactor88 <juan.perez@blankfactor.com>
2023-04-28 18:47:24 -0500

    ASA-428 <created model for overlay item >

---
+++ lib/modules/models/overlay_buton/overlay_button_model.freezed.dart
@@ -0,0 +1,155 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'overlay_button_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$CustomOverlayItemModel {
+  String get label => throw _privateConstructorUsedError;
+  String get imagePath => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $CustomOverlayItemModelCopyWith<CustomOverlayItemModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CustomOverlayItemModelCopyWith<$Res> {
+  factory $CustomOverlayItemModelCopyWith(CustomOverlayItemModel value,
+          $Res Function(CustomOverlayItemModel) then) =
+      _$CustomOverlayItemModelCopyWithImpl<$Res, CustomOverlayItemModel>;
+  @useResult
+  $Res call({String label, String imagePath});
+}
+
+/// @nodoc
+class _$CustomOverlayItemModelCopyWithImpl<$Res,
+        $Val extends CustomOverlayItemModel>
+    implements $CustomOverlayItemModelCopyWith<$Res> {
+  _$CustomOverlayItemModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? label = null,
+    Object? imagePath = null,
+  }) {
+    return _then(_value.copyWith(
+      label: null == label
+          ? _value.label
+          : label // ignore: cast_nullable_to_non_nullable
+              as String,
+      imagePath: null == imagePath
+          ? _value.imagePath
+          : imagePath // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_CustomOverlayItemModelCopyWith<$Res>
+    implements $CustomOverlayItemModelCopyWith<$Res> {
+  factory _$$_CustomOverlayItemModelCopyWith(_$_CustomOverlayItemModel value,
+          $Res Function(_$_CustomOverlayItemModel) then) =
+      __$$_CustomOverlayItemModelCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String label, String imagePath});
+}
+
+/// @nodoc
+class __$$_CustomOverlayItemModelCopyWithImpl<$Res>
+    extends _$CustomOverlayItemModelCopyWithImpl<$Res,
+        _$_CustomOverlayItemModel>
+    implements _$$_CustomOverlayItemModelCopyWith<$Res> {
+  __$$_CustomOverlayItemModelCopyWithImpl(_$_CustomOverlayItemModel _value,
+      $Res Function(_$_CustomOverlayItemModel) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? label = null,
+    Object? imagePath = null,
+  }) {
+    return _then(_$_CustomOverlayItemModel(
+      label: null == label
+          ? _value.label
+          : label // ignore: cast_nullable_to_non_nullable
+              as String,
+      imagePath: null == imagePath
+          ? _value.imagePath
+          : imagePath // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_CustomOverlayItemModel implements _CustomOverlayItemModel {
+  const _$_CustomOverlayItemModel(
+      {required this.label, required this.imagePath});
+
+  @override
+  final String label;
+  @override
+  final String imagePath;
+
+  @override
+  String toString() {
+    return 'CustomOverlayItemModel(label: $label, imagePath: $imagePath)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_CustomOverlayItemModel &&
+            (identical(other.label, label) || other.label == label) &&
+            (identical(other.imagePath, imagePath) ||
+                other.imagePath == imagePath));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, label, imagePath);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_CustomOverlayItemModelCopyWith<_$_CustomOverlayItemModel> get copyWith =>
+      __$$_CustomOverlayItemModelCopyWithImpl<_$_CustomOverlayItemModel>(
+          this, _$identity);
+}
+
+abstract class _CustomOverlayItemModel implements CustomOverlayItemModel {
+  const factory _CustomOverlayItemModel(
+      {required final String label,
+      required final String imagePath}) = _$_CustomOverlayItemModel;
+
+  @override
+  String get label;
+  @override
+  String get imagePath;
+  @override
+  @JsonKey(ignore: true)
+  _$$_CustomOverlayItemModelCopyWith<_$_CustomOverlayItemModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
