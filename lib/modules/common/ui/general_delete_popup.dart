5f9aeb2479e9ddaba6cb49e8ef760b50338ee523
juanfactor88 <juan.perez@blankfactor.com>
2023-05-09 11:37:22 -0500

    ASA-448 <updated widget with corrected behavior>

--- lib/modules/common/ui/general_delete_popup.dart
+++ lib/modules/common/ui/general_delete_popup.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:swagapp/generated/l10n.dart';
+import 'package:swagapp/modules/common/ui/primary_button.dart';
+import '../../models/ui_models/checkbox_model.dart';
 import '../utils/palette.dart';
 
 class GeneralDeletePopup extends StatefulWidget {
-  const GeneralDeletePopup({super.key, required this.title, required this.message, required this.options, this.model, required this.onSubmit});
+  const GeneralDeletePopup(
+      {super.key,
+      required this.title,
+      required this.message,
+      required this.options,
+      this.model,
+      required this.onSubmit});
 
   final String title;
   final String message;
-  final List<String> options;
+  final List<CheckboxModel> options;
   final dynamic model;
   final Function onSubmit;
 
@@ -15,31 +24,101 @@ class GeneralDeletePopup extends StatefulWidget {
 }
 
 class _GeneralDeletePopupState extends State<GeneralDeletePopup> {
-  bool? _isChecked = false;
-
   @override
   Widget build(BuildContext context) {
+    double deviceScreenRatio = MediaQuery.of(context).devicePixelRatio;
+    List<CheckboxModel> options = widget.options;
+    int selectedOption = -1;
+
     return Dialog(
-      child: Container(
-        color: Palette.current.blackSmoke,
-        child: Column(
-          children: [
-          Text(widget.title),
-          Text(widget.message),
-          SingleChildScrollView(
-            child: ListBody(
-              children: widget.options.map((option) => 
-              CheckboxListTile(
-                value: _isChecked, 
-                onChanged:(valueCheck) {
-                  setState(() {
-                    _isChecked=valueCheck;
-                  });
-                },
-                )).toList(),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          unselectedWidgetColor: Palette.current.darkGray,
+        ),
+        child: Container(
+          height: deviceScreenRatio * 220,
+          color: Palette.current.blackSmoke,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(31, 15, 31, 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                 Transform(
+                  transform: Matrix4.translationValues(deviceScreenRatio * 105, deviceScreenRatio * -1,0),
+                   child: SizedBox(
+                    height: deviceScreenRatio*10,
+                     child: IconButton(
+                      padding: const EdgeInsets.all(0),
+                       color: Palette.current.primaryNeonGreen,
+                       onPressed: () {
+                         Navigator.of(context).pop();
+                       },
+                       icon: const Icon(
+                         Icons.clear_outlined,
+                         size: 20,
+                       ),
+                     ),
+                   ),
+                 ),
+                Text(widget.title.toUpperCase(),
+                    style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                          fontFamily: "KnockoutCustom",
+                          fontSize: 44,
+                          fontWeight: FontWeight.w300,
+                          color: Palette.current.primaryNeonGreen,
+                        )),
+                SizedBox(
+                  height: deviceScreenRatio * 10,
+                ),
+                Text(widget.message,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontSize: 16,
+                          letterSpacing: 0.3,
+                          color: Palette.current.primaryWhiteSmoke,
+                        )),
+                SizedBox(
+                  height: deviceScreenRatio * 10,
+                ),
+                Expanded(child: StatefulBuilder(builder: (context, setState) {
+                  return ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: options.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(options[index].title,
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontSize: 16,
+                                      letterSpacing: 0.3,
+                                      color: Palette.current.primaryWhiteSmoke,
+                                    )),
+                        leading: Checkbox(
+                          value: selectedOption == index,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              selectedOption = value == true ? index : -1;
+                            });
+                          },
+                          activeColor: Palette.current.primaryNeonGreen,
+                          checkColor: Palette.current.primaryEerieBlack,
+                        ),
+                      );
+                    },
+                  );
+                })),
+                SizedBox(
+                  height: deviceScreenRatio * 10,
+                ),
+                PrimaryButton(
+                  title: S.of(context).razon_remove_btn,
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  type: PrimaryButtonType.green,
+                ),               
+              ],
             ),
-          )
-          ],
+          ),
         ),
       ),
     );
