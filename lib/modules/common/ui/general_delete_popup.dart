ddf3cd87be80315857b761aa03f0160292bcfc49
juanfactor88 <juan.perez@blankfactor.com>
2023-05-09 13:26:12 -0500

    ASA-448 <implemented action for calling the remove listing endpoint>

--- lib/modules/common/ui/general_delete_popup.dart
+++ lib/modules/common/ui/general_delete_popup.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:swagapp/generated/l10n.dart';
 import 'package:swagapp/modules/common/ui/primary_button.dart';
+import 'package:swagapp/modules/cubits/listing_for_sale/get_listing_for_sale_cubit.dart';
+import 'package:swagapp/modules/di/injector.dart';
+import 'package:swagapp/modules/models/listing_for_sale/listing_for_sale_model.dart';
+import '../../models/buy_for_sale_listing/buy_for_sale_listing_model.dart';
 import '../../models/ui_models/checkbox_model.dart';
 import '../utils/palette.dart';
+
 
 class GeneralDeletePopup extends StatefulWidget {
   const GeneralDeletePopup(
@@ -10,13 +15,13 @@ class GeneralDeletePopup extends StatefulWidget {
       required this.title,
       required this.message,
       required this.options,
-      this.model,
+      required this.model,
       required this.onSubmit});
 
   final String title;
   final String message;
   final List<CheckboxModel> options;
-  final dynamic model;
+  final BuyForSaleListingModel model;
   final Function onSubmit;
 
   @override
@@ -24,11 +29,21 @@ class GeneralDeletePopup extends StatefulWidget {
 }
 
 class _GeneralDeletePopupState extends State<GeneralDeletePopup> {
+  late ListingForSaleModel model;
+
   @override
   Widget build(BuildContext context) {
     double deviceScreenRatio = MediaQuery.of(context).devicePixelRatio;
     List<CheckboxModel> options = widget.options;
-    int selectedOption = -1;
+    int selectedOption = -1;    
+    model = ListingForSaleModel(
+      accountId: widget.model.profileId,
+      productItemId: widget.model.productItemId,
+      productItemName: widget.model.productItemName,
+      catalogItemId: widget.model.catalogItemId,
+      sold: false,
+      forSale: true,      
+    );
 
     return Dialog(
       child: Theme(
@@ -97,6 +112,7 @@ class _GeneralDeletePopupState extends State<GeneralDeletePopup> {
                           onChanged: (bool? value) {
                             setState(() {
                               selectedOption = value == true ? index : -1;
+                              model = model.copyWith(removedReason: options[index].title, status: "removed");                              
                             });
                           },
                           activeColor: Palette.current.primaryNeonGreen,
@@ -112,7 +128,12 @@ class _GeneralDeletePopupState extends State<GeneralDeletePopup> {
                 PrimaryButton(
                   title: S.of(context).razon_remove_btn,
                   onPressed: () {
-                    setState(() {});
+                    setState(() {
+                      getIt<ListingProfileCubit>().removeListingItem(model);
+                      getIt<ListingProfileCubit>().loadResults();
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop();
+                    });
                   },
                   type: PrimaryButtonType.green,
                 ),               
