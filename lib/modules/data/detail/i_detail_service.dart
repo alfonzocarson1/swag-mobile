ca9d1254deb3586be314d4e09d41c3ba42a31c6c
juanfactor88 <juan.perez@blankfactor.com>
2023-05-26 15:06:00 -0500

    ASA-1083 <added get availability status method>

--- lib/modules/data/detail/i_detail_service.dart
+++ lib/modules/data/detail/i_detail_service.dart
@@ -4,7 +4,8 @@ import '../../models/detail/detail_item_model.dart';
 abstract class IDetailService {
   Stream<String?> subscribeToAuthChanges();
   Future<DetailItemModel> itemDetail(String itemId);
-  Future<dynamic> notifyAvailability(String catalogoId);
+  Future<dynamic> notifyAvailability(String catalogId);
+  Future<dynamic> getAvailabilityStatus();
   Future<BuyForSaleListingResponseModel> buyForSaleListing(
       String catalogItemId);
 }
