e99329e1f9142bce0388ddc6e11215c36881c0a2
juanfactor88 <juan.perez@blankfactor.com>
2023-05-29 14:12:33 -0500

    ASA-1083 <added type of future>

--- lib/modules/data/detail/i_detail_service.dart
+++ lib/modules/data/detail/i_detail_service.dart
@@ -1,3 +1,5 @@
+import 'package:swagapp/modules/models/notify_when_available/profile_notify_list.dart';
+
 import '../../models/buy_for_sale_listing/buy_for_sale_listing_response_model.dart';
 import '../../models/detail/detail_item_model.dart';
 
@@ -5,7 +7,7 @@ abstract class IDetailService {
   Stream<String?> subscribeToAuthChanges();
   Future<DetailItemModel> itemDetail(String itemId);
   Future<dynamic> notifyAvailability(String catalogId);
-  Future<dynamic> getAvailabilityStatus();
+  Future<ProfileNotifyList> getAvailabilityStatus();
   Future<BuyForSaleListingResponseModel> buyForSaleListing(
       String catalogItemId);
 }
