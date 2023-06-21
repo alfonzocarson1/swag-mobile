7eb9f104fc445d58f92709ea05e7ce90c51426a1
juanfactor88 <juan.perez@blankfactor.com>
2023-06-20 21:03:52 -0500

    ASA-781 <update paywall widget>

--- lib/modules/common/ui/paywall_widget.dart
+++ lib/modules/common/ui/paywall_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:swagapp/modules/common/ui/discount_container_widget.dart';
 
 import '../../../generated/l10n.dart';
 import '../utils/palette.dart';
@@ -10,25 +11,66 @@ class PayWallWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      width: 800.0,  // the width you want
-      height: 1900.0,  // the height you want
+    
+    List<String> payWallConditionList =[
+      S.of(context).paywall_condition1,
+      S.of(context).paywall_condition2,
+      S.of(context).paywall_condition3,
+      S.of(context).paywall_condition4,
+      S.of(context).paywall_condition5,
+    ];
+
+    return  SizedBox(
+      width: MediaQuery.of(context).size.width,  // the width you want
+      height: MediaQuery.of(context).size.height * 0.6,  // the height you want
       child: ClipRect(
       child: BackdropFilter(
     
         filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-        child:  Column(
-          children: [
-            Text(S.of(context).my_collection,
-                      style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                          letterSpacing: 1,
-                          fontWeight: FontWeight.w300,
-                          fontFamily: "KnockoutCustom",
-                          fontSize: 28,
-                          color: Palette.current.primaryNeonGreen)),
-            Text('Subtitulo de prueba'),
-      
-          ],      
+        child:  Padding(
+          padding: const EdgeInsets.fromLTRB(30, 10, 30, 30),
+          child: Column(
+            children: [
+              Text(S.of(context).paywall_title.toUpperCase(),
+              textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                            letterSpacing: 1,
+                            fontWeight: FontWeight.w300,
+                            fontFamily: "KnockoutCustom",
+                            fontSize: 30,
+                            color: Palette.current.primaryNeonGreen)),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: payWallConditionList.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Container(     
+                    height: 20,                  
+                    margin:  const EdgeInsets.symmetric(vertical: 10),
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 90.0),
+                      minVerticalPadding: 0,          
+                      leading: SizedBox(
+                        height: 20,
+                        width: 20,
+                        child: Image.asset('assets/icons/list_green_check.png')),
+                      title: Text(payWallConditionList[index],
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall!
+                                          .copyWith(
+                                              fontWeight: FontWeight.w300,
+                                              color: Palette.current.primaryWhiteSmoke)),
+                    ),                    
+                  );
+                }
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+               const DiscountContainerWidget(),              
+            ],      
+          ),
         ),
     )));
   }
