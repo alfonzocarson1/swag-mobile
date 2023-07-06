f3ee64338245bfeb007340cb396b4b0600efc9f4
juanfactor88 <juan.perez@blankfactor.com>
2023-07-06 14:17:40 -0500

    ASA-2308 <fixed avatar shape>

--- lib/modules/pages/chats/widgets/chats_contacts.dart
+++ lib/modules/pages/chats/widgets/chats_contacts.dart
@@ -15,78 +15,79 @@ import '../../../constants/constants.dart';
 import '../../../di/injector.dart';
 
 class ChatsContact extends StatelessWidget {
-
   final String lastMessage;
   final ChatData chatData;
 
   const ChatsContact({
     super.key,
-    required this.lastMessage, 
+    required this.lastMessage,
     required this.chatData,
   });
 
   @override
   Widget build(BuildContext context) {
-    ProfileModel userProfile = getIt<PreferenceRepositoryService>().profileData();
+    ProfileModel userProfile =
+        getIt<PreferenceRepositoryService>().profileData();
     String userName = userProfile.username;
 
     int unreadMessages = this.chatData.channel.unreadMessageCount;
     bool hasUreadMessages = unreadMessages > 0;
     SendBirdChannelData channelData = this.getChannelData();
     List<Member> chatMembers = this.chatData.channel.members;
-    Member seller = chatMembers.where((Member member) => member.nickname != userName && member.nickname != swagBotNickName).toList().first;
+    Member seller = chatMembers
+        .where((Member member) =>
+            member.nickname != userName && member.nickname != swagBotNickName)
+        .toList()
+        .first;
 
-        
-    return ListTile(            
+    return ListTile(
       leading: _Avatar(
-        hasUreadMessages: hasUreadMessages, 
+        hasUreadMessages: hasUreadMessages,
         chatData: this.chatData,
       ),
       title: Text(
         '@${seller.nickname.capitalize()} - ${channelData.listingProductName}',
         style: Theme.of(context).textTheme.bodySmall!.copyWith(
-          fontWeight: (hasUreadMessages) ? FontWeight.w300 : FontWeight.w300,
-          fontSize: 20,
-          color: (hasUreadMessages) ? Colors.white : Palette.current.grey,
-        ),
+              fontWeight:
+                  (hasUreadMessages) ? FontWeight.w300 : FontWeight.w300,
+              fontSize: 20,
+              color: (hasUreadMessages) ? Colors.white : Palette.current.grey,
+            ),
       ),
       subtitle: Text(
         this.lastMessage,
         maxLines: 1,
         style: Theme.of(context).textTheme.bodySmall!.copyWith(
-          fontSize: 15,
-          letterSpacing: 0.3,
-          color: Palette.current.grey,
-          overflow: TextOverflow.ellipsis
-        ),
+            fontSize: 15,
+            letterSpacing: 0.3,
+            color: Palette.current.grey,
+            overflow: TextOverflow.ellipsis),
       ),
-      onTap: ()=> Navigator.push(
-        context, 
+      onTap: () => Navigator.push(
+        context,
         MaterialPageRoute(
-          builder: (BuildContext context)=> ChatPage(chatData: this.chatData),
+          builder: (BuildContext context) => ChatPage(chatData: this.chatData),
         ),
       ),
     );
   }
 
-  SendBirdChannelData  getChannelData() {
-
-    if(this.chatData.channel.data!.isNotEmpty) {
-
-      String stringData = json.encode(this.chatData.channel.data!.replaceAll("'", '"'));
+  SendBirdChannelData getChannelData() {
+    if (this.chatData.channel.data!.isNotEmpty) {
+      String stringData =
+          json.encode(this.chatData.channel.data!.replaceAll("'", '"'));
       String formatedData = stringData.replaceAll('\\', "");
-      Map<String, dynamic> data  = json.decode(formatedData.substring(1, formatedData.length - 1));
+      Map<String, dynamic> data =
+          json.decode(formatedData.substring(1, formatedData.length - 1));
 
       return SendBirdChannelData.fromJson(data);
-    }
-    else {
+    } else {
       return SendBirdChannelData(topicId: '', listingProductName: '');
     }
-  } 
+  }
 }
 
 class _Avatar extends StatelessWidget {
-  
   const _Avatar({
     super.key,
     required this.hasUreadMessages,
@@ -98,33 +99,32 @@ class _Avatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     bool isCoverChannelNull = this.chatData.channel.coverUrl == null;
 
     return CircleAvatar(
-      backgroundColor: (this.hasUreadMessages) 
-      ? Palette.current.primaryNeonPink
-      : Colors.transparent,
-      maxRadius: 22,
-      child: (isCoverChannelNull) 
-      ? Image.asset(
-          AppImages.avatar,
-          scale: 2.5,
-        )
-      : Container(
-          margin: const EdgeInsets.all(2),
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: CachedNetworkImage(
-              fit: BoxFit.cover,
-              imageUrl: this.chatData.channel.coverUrl!,
-            ),  
-          ),
-        )
-    );
+        backgroundColor: (
+          this.hasUreadMessages)
+            ? Palette.current.primaryNeonPink
+            : Colors.transparent,
+        maxRadius: 22,
+        child: (isCoverChannelNull)
+            ? Image.asset(
+                AppImages.avatar,
+                scale: 2.5,
+              )
+            : Container(
+                margin: const EdgeInsets.all(2),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                child: ClipOval(
+                  child: CachedNetworkImage(
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                    imageUrl: this.chatData.channel.coverUrl!,
+                  ),
+                ),
+              ));
   }
 }
-
