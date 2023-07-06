088eae6207a0fbddb09b4bb0c6c5f1c20208a57b
juanfactor88 <juan.perez@blankfactor.com>
2023-07-06 11:08:38 -0500

    ASA-2345 <created paywall cubit>

---
+++ lib/modules/cubits/paywall/paywall_cubit_state.dart
@@ -0,0 +1,9 @@
+part of 'paywall_cubit.dart';
+
+@freezed
+class PaywallCubitState with _$PaywallCubitState {
+  const factory PaywallCubitState.initial() = PaywallCubitStateInitial;
+  const factory PaywallCubitState.progress() = PaywallCubitStateProgress;
+  const factory PaywallCubitState.success() = PaywallCubitStateSuccess;
+  const factory PaywallCubitState.error(String errorMessage) = PaywallCubitStateError;
+}
