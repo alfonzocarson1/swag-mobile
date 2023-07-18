1811fd0ba48553b3f2130c32d0904e91adb23ae9
drezdenschock <drezdenschock@gmail.com>
2023-07-18 08:27:11 -0500

    ASA-210 ASA-474 ASA-475 ASA-476

--- lib/modules/common/ui/account_info_head.dart
+++ lib/modules/common/ui/account_info_head.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:swagapp/modules/common/ui/profile_username_rating.dart';
 import 'package:swagapp/modules/common/utils/palette.dart';
 
 import '../../data/shared_preferences/shared_preferences_service.dart';
@@ -36,68 +37,11 @@ class _AccountInfoHeaderWidgetState extends State<AccountInfoHeaderWidget> {
           const SizedBox(
             height: 20,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10.0),
-                child: Text('@${profileData.username}',
-                    style: Theme.of(context).textTheme.displayMedium!.copyWith(
-                        fontFamily: "KnockoutCustom",
-                        fontSize: 33,
-                        letterSpacing: 1.0,
-                        fontWeight: FontWeight.w300,
-                        color: Palette.current.light4)),
-              ),
-              if (profileData.kycverified == true) ...[
-                const SizedBox(
-                  width: 5,
-                ),
-                Image.asset(
-                  'assets/images/Verifyindicator.png',
-                  width: 25,
-                  height: 25,
-                ),
-              ],
-            ],
+          ProfileUsernameRating(
+            username: profileData.username,
+            kycVerified: profileData.kycverified ?? false,
+            rating: profileData.listingsRating,
           ),
-          if (profileData.listingsRating >= 1) ...[
-            const SizedBox(
-              height: 16,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Palette.current.primaryNeonGreen,
-                borderRadius: BorderRadius.circular(100),
-              ),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 12,
-                vertical: 6,
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ImageIcon(
-                    const AssetImage("assets/icons/thumbs-up.png"),
-                    size: 20,
-                    color: Palette.current.primaryNero,
-                  ),
-                  const SizedBox(width: 6),
-                  Text(
-                    NumberFormat.decimalPattern()
-                        .format(profileData.listingsRating.toInt()),
-                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        fontFamily: "KnockoutCustom",
-                        fontSize: 16,
-                        color: Palette.current.primaryNero,
-                        fontWeight: FontWeight.w300,
-                        letterSpacing: 0.3),
-                  ),
-                ],
-              ),
-            ),
-          ]
         ],
       ),
     );
--- lib/modules/pages/public_profile/public_profile_page.dart
+++ lib/modules/pages/public_profile/public_profile_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:swagapp/modules/common/ui/profile_username_rating.dart';
 import 'package:swagapp/modules/common/ui/pushed_header.dart';
+import 'package:swagapp/modules/common/ui/user_avatar.dart';
+import 'package:swagapp/modules/common/utils/stateful_wrapper.dart';
+import 'package:swagapp/modules/models/profile/public_profile.dart';
 
+import '../../common/ui/loading.dart';
 import '../../common/utils/custom_route_animations.dart';
+import '../../common/utils/palette.dart';
+import '../../cubits/public_profile/public_profile_cubit.dart';
 
 class PublicProfilePage extends StatelessWidget {
   static const name = "/publicProfile";
@@ -16,11 +24,71 @@ class PublicProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PushedHeader(
-        title: Text("Public Profile [WIP]"),
-        showBackButton: true,
+    return StatefulWrapper(
+      onInit: (context) {
+        context.read<PublicProfileCubit>().loadProfile(
+              profileId,
+            );
+      },
+      child: Scaffold(
+        appBar: PushedHeader(
+          showBackButton: true,
+          height: 70,
+          backgroundColor: Colors.transparent,
+          suffixIconButton: IconButton(
+            icon: ImageIcon(
+              const AssetImage("assets/images/more-horizontal.png"),
+              color: Palette.current.primaryWhiteSmoke,
+            ),
+            onPressed: () {},
+          ),
+        ),
+        backgroundColor: Palette.current.primaryNero,
+        body: BlocConsumer<PublicProfileCubit, PublicProfileState>(
+          listener: (context, state) {
+            if (state.isLoadingWithoutPreviousData && !Loading.isVisible()) {
+              Loading.show(context);
+            } else if (!state.isLoading && Loading.isVisible()) {
+              Loading.hide(context);
+            }
+          },
+          builder: (context, state) {
+            return state.when(
+              error: (e, previousData) => Center(
+                child: Text(
+                  "Error loading profile: $e",
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: Palette.current.primaryNeonPink,
+                      ),
+                ),
+              ),
+              loading: (previousData) => previousData == null
+                  ? Container()
+                  : buildBody(context, previousData),
+              loaded: (data) => buildBody(context, data),
+            );
+          },
+        ),
       ),
+    );
+  }
+
+  buildBody(BuildContext context, PublicProfile profile) {
+    return Column(
+      children: [
+        const SizedBox(height: 18),
+        UserAvatar(
+          useAvatar: profile.useAvatar,
+          avatarUrl: profile.avatarUrl,
+          radius: 125 / 2,
+        ),
+        const SizedBox(height: 14),
+        ProfileUsernameRating(
+          username: profile.username ?? "NULL",
+          kycVerified: profile.kycverified ?? false,
+          rating: profile.listingsRating ?? 0,
+        )
+      ],
     );
   }
 }
