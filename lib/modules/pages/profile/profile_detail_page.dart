f685d60ec75b8f189318e4f78d07bb80fa0285cb
Alfonzo Carson <alfonzocarson03@gmail.com>
2023-07-25 17:15:59 -0500

    ASA-796 implemented.

--- lib/modules/pages/profile/profile_detail_page.dart
+++ lib/modules/pages/profile/profile_detail_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:swagapp/modules/common/ui/primary_button.dart';
+import 'package:swagapp/modules/cubits/profile/get_profile_cubit.dart';
 import 'package:swagapp/modules/data/shared_preferences/shared_preferences_service.dart';
 import 'package:swagapp/modules/di/injector.dart';
 import 'package:swagapp/modules/models/profile/profile_model.dart';
+import 'package:swagapp/modules/pages/profile/update_name_page.dart';
 import '../../../generated/l10n.dart';
 import '../../common/ui/pushed_header.dart';
 import '../../common/utils/custom_route_animations.dart';
@@ -24,15 +27,15 @@ class ProfileDetailPage extends StatefulWidget {
 }
 
 class _ProfileDetailPage extends State<ProfileDetailPage> {
+
   @override
   void initState() {
     super.initState();
+
   }
 
   @override
   Widget build(BuildContext context) {
-    ProfileModel profileData =
-        getIt<PreferenceRepositoryService>().profileData();
 
     return Scaffold(
       appBar: PushedHeader(
@@ -50,134 +53,143 @@ class _ProfileDetailPage extends State<ProfileDetailPage> {
         height: 70,
       ),
       backgroundColor: Palette.current.primaryNero,
-      body: Column(
-        children: [
-          Expanded(
-            child: LayoutBuilder(builder: (context, viewportConstraints) {
-              return SingleChildScrollView(
-                physics: const ClampingScrollPhysics(),
-                child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minHeight: viewportConstraints.maxHeight,
-                    ),
-                    child: Column(
-                      children: [
-                        _selectTile(
-                            'assets/icons/BlockUserWhite.png',
-                            S.of(context).profile_name_title,
-                            '${profileData.firstName ?? ''} ${profileData.lastName ?? ''}',
-                            () {},
-                            true,
-                            '',
-                            '',
-                            true),
-                        SizedBox(
-                          height: 0.2,
-                          child: Container(
-                            color: Palette.current.grey,
-                          ),
+      body: BlocBuilder<ProfileCubit, ProfileCubitState>(
+        builder: (context, state) {
+          ProfileModel profileData =
+          getIt<PreferenceRepositoryService>().profileData();
+          return Column(
+            children: [
+              Expanded(
+                child: LayoutBuilder(builder: (context, viewportConstraints) {
+                  return SingleChildScrollView(
+                    physics: const ClampingScrollPhysics(),
+                    child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minHeight: viewportConstraints.maxHeight,
                         ),
-                        _selectTile(
-                            'assets/icons/account_icon.png',
-                            S.of(context).profile_username_title,
-                            profileData.username,
-                            () {},
-                            profileData.kycverified! ? true : false,
-                            '',
-                            profileData.kycverified! ? 'assets/icons/Verifiedindicator_icon.png' : '',
-                            false),
-                        SizedBox(
-                          height: 0.2,
-                          child: Container(
-                            color: Palette.current.grey,
-                          ),
-                        ),
-                        _selectTile(
-                            'assets/icons/security_icon.png',
-                            S.of(context).profile_password_title,
-                            '*************', () {
-                          getIt<PreferenceRepositoryService>()
-                              .saveForgotPasswordFlow(true);
-                          Navigator.of(context, rootNavigator: true)
-                              .push(ForgotPasswordPage.route(true));
-                        }, true, '', '', true),
-                        SizedBox(
-                          height: 0.2,
-                          child: Container(
-                            color: Palette.current.grey,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 0.2,
-                          child: Container(
-                            color: Palette.current.grey,
-                          ),
-                        ),
-                        _selectTile(
-                            'assets/icons/email.png',
-                            S.of(context).profile_email_title,
-                            profileData.email,
-                            () {},
-                            false,
-                             profileData.emailVerified
-                                ? S.of(context).email_verified
-                                : S.of(context).unverified,
-                            '',
-                            false),
-                        SizedBox(
-                          height: 0.2,
-                          child: Container(
-                            color: Palette.current.grey,
-                          ),
-                        ),
-                        _selectTile(
-                            'assets/icons/contact_us_icon.png',
-                            S.of(context).profile_mobile_number_title,
-                            profileData.phoneNumber,
-                            () {},
-                            true,
-                            '',
-                            '',
-                            true),
-                        SizedBox(
-                          height: 0.2,
-                          child: Container(
-                            color: Palette.current.grey,
-                          ),
-                        ),
-                        _selectTile(
-                            'assets/icons/trash.png',
-                            S.of(context).profile_delete_title,
-                            '',
-                            () {},
-                            true,
-                            '',
-                            '',
-                            true),
-                        SizedBox(
-                          height: 0.2,
-                          child: Container(
-                            color: Palette.current.grey,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 150,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          child: PrimaryButton(
-                            title: S.of(context).sign_out.toUpperCase(),
-                            onPressed: () {},
-                            type: PrimaryButtonType.pink,
-                          ),
-                        )
-                      ],
-                    )),
-              );
-            }),
-          ),
-        ],
-      ),
+                        child: Column(
+                          children: [
+                            _selectTile(
+                                'assets/icons/BlockUserWhite.png',
+                                S.of(context).profile_name_title,
+                                '${profileData.firstName ?? ''} ${profileData.lastName ?? ''}',
+                                    () {
+                                  Navigator.of(context, rootNavigator: true)
+                                      .push(UpdateNamePage.route());
+                                },
+                                true,
+                                '',
+                                '',
+                                true),
+                            SizedBox(
+                              height: 0.2,
+                              child: Container(
+                                color: Palette.current.grey,
+                              ),
+                            ),
+                            _selectTile(
+                                'assets/icons/account_icon.png',
+                                S.of(context).profile_username_title,
+                                profileData.username,
+                                    () {},
+                                profileData.kycverified! ? true : false,
+                                '',
+                                profileData.kycverified! ? 'assets/icons/Verifiedindicator_icon.png' : '',
+                                false),
+                            SizedBox(
+                              height: 0.2,
+                              child: Container(
+                                color: Palette.current.grey,
+                              ),
+                            ),
+                            _selectTile(
+                                'assets/icons/security_icon.png',
+                                S.of(context).profile_password_title,
+                                '*************', () {
+                              getIt<PreferenceRepositoryService>()
+                                  .saveForgotPasswordFlow(true);
+                              Navigator.of(context, rootNavigator: true)
+                                  .push(ForgotPasswordPage.route(true));
+                            }, true, '', '', true),
+                            SizedBox(
+                              height: 0.2,
+                              child: Container(
+                                color: Palette.current.grey,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 0.2,
+                              child: Container(
+                                color: Palette.current.grey,
+                              ),
+                            ),
+                            _selectTile(
+                                'assets/icons/email.png',
+                                S.of(context).profile_email_title,
+                                profileData.email,
+                                    () {},
+                                false,
+                                profileData.emailVerified
+                                    ? S.of(context).email_verified
+                                    : S.of(context).unverified,
+                                '',
+                                false),
+                            SizedBox(
+                              height: 0.2,
+                              child: Container(
+                                color: Palette.current.grey,
+                              ),
+                            ),
+                            _selectTile(
+                                'assets/icons/contact_us_icon.png',
+                                S.of(context).profile_mobile_number_title,
+                                profileData.phoneNumber,
+                                    () {},
+                                true,
+                                '',
+                                '',
+                                true),
+                            SizedBox(
+                              height: 0.2,
+                              child: Container(
+                                color: Palette.current.grey,
+                              ),
+                            ),
+                            _selectTile(
+                                'assets/icons/trash.png',
+                                S.of(context).profile_delete_title,
+                                '',
+                                    () {},
+                                true,
+                                '',
+                                '',
+                                true),
+                            SizedBox(
+                              height: 0.2,
+                              child: Container(
+                                color: Palette.current.grey,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 150,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: PrimaryButton(
+                                title: S.of(context).sign_out.toUpperCase(),
+                                onPressed: () {},
+                                type: PrimaryButtonType.pink,
+                              ),
+                            )
+                          ],
+                        )),
+                  );
+                }),
+              ),
+            ],
+          );
+        }
+      )
     );
   }
 
@@ -199,33 +211,21 @@ class _ProfileDetailPage extends State<ProfileDetailPage> {
             size: 25,
             color: Colors.white,
           ),
-          visualDensity: VisualDensity(vertical: 2),
-          title: subTitle != ''
-              ? Padding(
+       //   visualDensity: VisualDensity(vertical: 2),
+          title: Padding(
                   padding: const EdgeInsets.only(bottom: 3),
                   child: Text(title,
                       style: Theme.of(context).textTheme.bodySmall!.copyWith(
                           fontWeight: FontWeight.w400,
                           color: Palette.current.primaryWhiteSmoke,
                           fontSize: 16)),
-                )
-              : SizedBox(
-                  height: 60,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(title,
-                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                            fontWeight: FontWeight.w400,
-                            color: Palette.current.primaryWhiteSmoke,
-                            fontSize: 16)),
-                  )),
-          subtitle: subTitle != ''
-              ? Text(subTitle,
+                ),
+          subtitle: Text(subTitle,
                   style: Theme.of(context)
                       .textTheme
                       .bodySmall!
-                      .copyWith(color: Palette.current.grey, fontSize: 14))
-              : null,
+                      .copyWith(color: Palette.current.grey, fontSize: 14)),
+
           trailing: showRightIcon
               ? hasArrowIcon
                   ? Icon(
---
+++ lib/modules/pages/profile/update_name_page.dart
@@ -0,0 +1,232 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:swagapp/modules/blocs/update_profile_bloc/update_profile_bloc.dart';
+import 'package:swagapp/modules/common/ui/custom_app_bar.dart';
+import 'package:swagapp/modules/common/ui/custom_text_form_field.dart';
+import 'package:swagapp/modules/common/ui/primary_button.dart';
+import 'package:swagapp/modules/common/ui/pushed_header.dart';
+import 'package:swagapp/modules/common/utils/custom_route_animations.dart';
+import 'package:swagapp/modules/common/utils/palette.dart';
+import 'package:swagapp/modules/common/utils/size_helper.dart';
+import 'package:swagapp/modules/data/shared_preferences/shared_preferences_service.dart';
+import 'package:swagapp/modules/di/injector.dart';
+import 'package:swagapp/modules/models/profile/profile_model.dart';
+import 'package:swagapp/modules/models/update_profile/update_profile_payload_model.dart';
+
+import '../../../generated/l10n.dart';
+import '../../common/ui/dynamic_toast_messages.dart';
+import '../../common/ui/loading.dart';
+
+class UpdateNamePage extends StatefulWidget {
+  static const name = '/UpdateNamePage';
+
+  const UpdateNamePage({super.key});
+
+  static Route route() => PageRoutes.material(
+    settings: const RouteSettings(name: name),
+    builder: (context) => const UpdateNamePage(),
+  );
+
+  @override
+  State<UpdateNamePage> createState() => _UpdateNamePage();
+}
+
+class _UpdateNamePage extends State<UpdateNamePage> {
+  late ResponsiveDesign _responsiveDesign;
+  final FocusNode _firstNameNode = FocusNode();
+  final _firstNameController = TextEditingController();
+  String? errorFirstText;
+  bool readOnly = false;
+
+  final FocusNode _lastNameNode = FocusNode();
+  final _lastNameController = TextEditingController();
+  String? errorSecondText;
+
+  Color _firstNameBorder = Palette.current.primaryWhiteSmoke;
+  Color _lastNameBorder = Palette.current.primaryWhiteSmoke;
+  late ProfileModel profileData;
+  bool enableSaveButton = false;
+  @override
+  void initState() {
+    super.initState();
+    profileData = getIt<PreferenceRepositoryService>().profileData();
+    _firstNameController.text = profileData.firstName ?? '';
+    _lastNameController.text = profileData.lastName ?? '';
+
+    _firstNameNode.addListener(() {
+      if (_firstNameNode.hasFocus) {
+        setState(() {
+          errorFirstText = null;
+          errorSecondText = null;
+        });
+      }
+      setState(() {
+
+        _firstNameBorder = _firstNameNode.hasFocus
+            ? Palette.current.primaryNeonGreen
+            : Palette.current.primaryWhiteSmoke;
+      });
+    });
+    _lastNameNode.addListener(() {
+      if (_lastNameNode.hasFocus) {
+        setState(() {
+          errorFirstText = null;
+          errorSecondText = null;
+        });
+      }
+      setState(() {
+
+        _lastNameBorder = _lastNameNode.hasFocus
+            ? Palette.current.primaryNeonGreen
+            : Palette.current.primaryWhiteSmoke;
+      });
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _responsiveDesign = ResponsiveDesign(context);
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        resizeToAvoidBottomInset: true,
+        backgroundColor: Palette.current.primaryNero,
+        appBar: PushedHeader(
+          showBackButton: true,
+          title: Align(
+            alignment: Alignment.centerRight,
+            child: Text(S.of(context).profile_name.toUpperCase(),
+                style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    letterSpacing: 1,
+                    fontWeight: FontWeight.w300,
+                    fontFamily: "KnockoutCustom",
+                    fontSize: 30,
+                    color: Palette.current.primaryNeonGreen)),
+          ),
+          height: 70,
+        ),
+        body: BlocListener<UpdateProfileBloc, UpdateProfileState>(
+            listener: (context, state) => state.maybeWhen(
+              orElse: () {
+                return null;
+              },
+              updated: () {
+                Loading.hide(context);
+                Navigator.of(context).pop();
+                return null;
+              },
+              initial: () {
+                return Loading.show(context);
+              },
+              error: (message) => {
+
+                Loading.hide(context),
+                // Dialogs.showOSDialog(context, 'Error', message, 'OK', () {})
+              },
+            ),
+            child: _getBody()));
+  }
+
+  GestureDetector _getBody() {
+    return GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
+          _firstNameNode.unfocus();
+          _lastNameNode.unfocus();
+        },
+        child: Stack(children: [
+          LayoutBuilder(builder: (context, viewportConstraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SafeArea(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const SizedBox(
+                          height: 30,
+                        ),
+
+                        CustomTextFormField(
+                            textCapitalization: TextCapitalization.sentences,
+                            onChanged: (text){
+                              setState(() {
+                                errorFirstText = null;
+                              });
+                            },
+                            errorText: errorFirstText,
+                            borderColor: _firstNameBorder,
+                            autofocus: false,
+                            labelText: S.of(context).profile_first_name,
+                            focusNode: _firstNameNode,
+                            controller: _firstNameController,
+                            secure: false,
+                            inputType: TextInputType.text),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        CustomTextFormField(
+                          textCapitalization: TextCapitalization.sentences,
+                            onChanged: (text) {
+                              setState(() {
+                                errorSecondText = null;
+                              });
+                            },
+                            errorText: errorSecondText,
+                            borderColor: _lastNameBorder,
+                            autofocus: false,
+                            labelText: S.of(context).last_name,
+                            focusNode: _lastNameNode,
+                            controller: _lastNameController,
+                            secure: false,
+                            inputType: TextInputType.text),
+                         SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.49,
+                        ),
+                        PrimaryButton(
+                          title: 'SAVE',
+                          onPressed: () {
+                             showErrors();
+                            if (enableSaveButton) {
+                              context.read<UpdateProfileBloc>().add(UpdateProfileEvent.update(
+                                UpdateProfilePayloadModel(
+                                  firstName: _firstNameController.text,
+                                  lastName: _lastNameController.text
+                                )
+                              ));
+                            }
+                          },
+                          type: PrimaryButtonType.green,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }),
+        ]));
+  }
+
+  void showErrors() {
+    setState(() {
+      errorFirstText = _firstNameController.text.isEmpty
+          ? S.of(context).required_field
+          :  null;
+
+      errorSecondText = _lastNameController.text.isEmpty
+          ? S.of(context).required_field
+          : null;
+
+      enableSaveButton = (_firstNameController.text.isNotEmpty && _lastNameController.text.isNotEmpty) ? true: false;
+    });
+  }
+}
