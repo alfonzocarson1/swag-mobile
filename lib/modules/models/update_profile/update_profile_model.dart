6f7472968e312447ed2b63078046cf07aeebfd27
Alfonzo Carson <alfonzocarson03@gmail.com>
2023-07-28 17:06:12 -0500

    ASA-799 has implemented.

--- lib/modules/models/update_profile/update_profile_model.dart
+++ lib/modules/models/update_profile/update_profile_model.dart
@@ -8,14 +8,28 @@ part 'update_profile_model.g.dart';
 @freezed
 class UpdateProfileModel with _$UpdateProfileModel {
   const factory UpdateProfileModel({
-    required final String accountId,
-    required final String username,
-    required final double listingsRating,
-    required final String phoneNumber,
-    required final String email,
+     final String? accountId,
+     final String? username,
+     final double? listingsRating,
+     final String? phoneNumber,
+     final String? email,
     List<AddressesPayloadModel>? addresses,
+    final bool? hasActiveSubscription,
+    final Status? status,
   }) = _UpdateProfileModel;
 
   factory UpdateProfileModel.fromJson(Map<String, dynamic> json) =>
       _$UpdateProfileModelFromJson(json);
+}
+
+@freezed
+class Status with _$Status {
+  const factory Status({
+    final String? errorCode,
+    final String? errorMessage
+}) = _Status;
+
+  factory Status.fromJson(Map<String, dynamic> json) =>
+      _$StatusFromJson(json);
+
 }
