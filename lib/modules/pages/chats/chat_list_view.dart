e232b95c650fb4cd95e8ac7d8c11af9187debc83
juanfactor88 <juan.perez@blankfactor.com>
2023-08-04 18:26:32 -0500

    ASA-2761 <added validation to unknown channel>

--- lib/modules/pages/chats/chat_list_view.dart
+++ lib/modules/pages/chats/chat_list_view.dart
@@ -75,7 +75,10 @@ class _ChatListPageState extends State<ChatListPage> {
                   itemCount: channels.length,
                   itemBuilder: (context, index) {
                     final channel = channels[index];
-                    String lastMessage= "";
+                    if(channel.name == "Push Notification Tester"){
+                      return const SizedBox.shrink();
+                    }else{
+                       String lastMessage= "";
                     if(channel.customType == ChatType.buyWorkflow.textValue && channel.lastMessage != null){
                       (channel.lastMessage?.data != "" ) ? lastMessage = getBuyFlowLastMessage(channel.lastMessage?.data) :  lastMessage = channel.lastMessage!.message ;
                     }else if(channel.customType == ChatType.buyWorkflow.textValue && channel.lastMessage == null){
@@ -86,6 +89,10 @@ class _ChatListPageState extends State<ChatListPage> {
                     }          
                     ChatData chatData = ChatData(messages: messages, channel: channel);
                     return ChatsContact(lastMessage: lastMessage, chatData: chatData);
+
+                    }
+                    
+                   
               
                         }),
               ),
