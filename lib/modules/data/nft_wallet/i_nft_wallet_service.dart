2597a42470297f8cf33ffcb9de9e9bcb8de0c1fb
Tarek Mohamed Abdalla <drezdenschock@gmail.com>
2023-08-10 03:05:33 +0300

    nft

--- lib/modules/data/nft_wallet/i_nft_wallet_service.dart
+++ lib/modules/data/nft_wallet/i_nft_wallet_service.dart
@@ -1,5 +1,7 @@
 import 'package:swagapp/modules/models/nft_wallet/nft_wallet.dart';
 
 abstract class INftWalletService {
-  Future<List<NftWalletModel>> getNftWallets();
+  Future<List<NftWalletModel>> getNftWallets(String accountId);
+  Future<NftWalletModel> verifyNftWallet(String accountId, String walletNft);
+  Future<NftWalletModel> createNftWallet(String accountId, String walletNft);
 }
