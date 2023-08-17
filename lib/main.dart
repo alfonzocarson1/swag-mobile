9a0c976f5818a6603df63c808caddcabb6c7444f
juanfactor88 <juan.perez@blankfactor.com>
2023-08-17 18:09:16 -0500

    removen unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sendbird_chat_sdk/sendbird_chat_sdk.dart';
 
+
 import 'app.dart';
 import 'modules/api/app_config.dart';
-import 'modules/common/utils/context_service.dart';
 import 'modules/common/utils/utils.dart';
-import 'modules/constants/constants.dart';
 import 'modules/data/secure_storage/storage_repository_service.dart';
 import 'modules/data/shared_preferences/shared_preferences_service.dart';
 import 'modules/di/injector.dart';
 import 'modules/notifications_providers/local_notifications_providers.dart';
 import 'modules/notifications_providers/push_notifications_providers.dart';
-import 'modules/pages/alert/alert_page.dart';
 
 Future<void> main() async {
   if (kReleaseMode) {
@@ -37,6 +35,7 @@ Future<void> main() async {
   String sendbirdAppId = getIt<AppConfig>().sendBirdAppId;
   SendbirdChat.init(appId: sendbirdAppId);
   getIt<StorageRepositoryService>().initialize();
+ 
   initFiltersAndSorts();
   initUtilsPreference();
   try {
@@ -46,6 +45,7 @@ Future<void> main() async {
       print('initNotifications error message: $e');
     }
   }
+   
   return _runApp();
 }
 
