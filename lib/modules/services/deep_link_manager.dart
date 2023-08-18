95ba8feea4259227a3493541b1eb4c339cd2df7d
juanfactor88 <juan.perez@blankfactor.com>
2023-08-18 15:23:18 -0500

    ASA-2297 <added logged validation to deeplink>

--- lib/modules/services/deep_link_manager.dart
+++ lib/modules/services/deep_link_manager.dart
@@ -2,14 +2,17 @@ import 'dart:async';
 
 import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
+import 'package:swagapp/modules/pages/login/create_account_page.dart';
 
 import '../common/utils/context_service.dart';
+import '../data/shared_preferences/shared_preferences_service.dart';
 import '../di/injector.dart';
 import '../pages/add/buy/preview_buy_for_sale.dart';
 
 class DeepLinkHandler {
   final AppLinks _appLinks = AppLinks();
   StreamSubscription<Uri>? _linkSubscription;
+  
 
 
   void init() async {
@@ -38,14 +41,25 @@ class DeepLinkHandler {
 
 
   void openAppLink(Uri uri) {
+    bool isLogged = getIt<PreferenceRepositoryService>().isLogged();
     var uriFragment = uri;
     String productId = getProductNumber(uriFragment.toString());
-
-   getIt<ContextService>().rootNavigatorKey.currentState?.push(
+   
+   if(isLogged == true){
+    getIt<ContextService>().rootNavigatorKey.currentState?.push(
     MaterialPageRoute(
       builder: (context) => BuyPreviewPage(productItemId: productId,)
       )
     );
+   }
+   else{
+   getIt<ContextService>().rootNavigatorKey.currentState?.push(
+    MaterialPageRoute(
+      builder: (context) => const CreateAccountPage()
+      )  
+   );  
+   }
+   
   }
 
   getProductNumber(String url){
