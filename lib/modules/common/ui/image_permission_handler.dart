641a09ee9c9880acf6e5b0e90cb4d2b559c62b02
juanfactor88 <juan.perez@blankfactor.com>
2023-08-31 16:38:10 -0500

    ASA-3087 <removed unecessary import>

--- lib/modules/common/ui/image_permission_handler.dart
+++ lib/modules/common/ui/image_permission_handler.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
