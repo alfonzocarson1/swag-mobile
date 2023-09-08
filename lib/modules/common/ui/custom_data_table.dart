db70d1a33d5961c195be6a7d054c75ab238e0223
alfonzocarson1 <lfnzcarson@gmail.com>
2023-09-08 15:11:34 -0500

    ASA-2488 fixed

--- lib/modules/common/ui/custom_data_table.dart
+++ lib/modules/common/ui/custom_data_table.dart
@@ -117,136 +117,136 @@ class _CustomDataTableState extends State<CustomDataTable> {
             columnSpacing: (height <= 840) ? 20 : 35,
             columns: [
               DataColumn(
-                label: SizedBox(
-                  width: 100,
-                  child: Row(
-                    children: [
-                      Flexible(
-                        child: Text(
-                          S.of(context).sale_data,
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .copyWith(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w300,
-                              color: Palette.current.white),
+                label: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _ascendingCondition = false;
+                      _ascendingPrice = false;
+                      if (_ascendingDate) {
+                        histories.sort((a, b) => a.updatedDate
+                            .toDateTime()
+                            .compareTo(b.updatedDate.toDateTime()));
+                      } else {
+                        histories.sort((a, b) => b.updatedDate
+                            .toDateTime()
+                            .compareTo(a.updatedDate.toDateTime()));
+                      }
+                      if (!_ascendingDate) {
+                        _ascendingDate = true;
+                      } else {
+                        _ascendingDate = false;
+                      }
+                    });
+                  },
+                  child: SizedBox(
+                    width: 100,
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: Text(
+                            S.of(context).sale_data,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w300,
+                                    color: Palette.current.white),
+                          ),
                         ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _ascendingCondition = false;
-                            _ascendingPrice = false;
-                            if (_ascendingDate) {
-                              histories.sort((a, b) => a.updatedDate.toDateTime()
-                                  .compareTo(b.updatedDate.toDateTime()));
-                            } else {
-                              histories.sort((a, b) => b.updatedDate.toDateTime()
-                                  .compareTo(a.updatedDate.toDateTime()));
-                            }
-                            if (!_ascendingDate) {
-                              _ascendingDate = true;
-                            } else {
-                              _ascendingDate = false;
-                            }
-                          });
-                        },
-                        child: Icon(
+                        Icon(
                             _ascendingDate
                                 ? Icons.expand_more
                                 : Icons.expand_less,
                             size: 20),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
               DataColumn(
-                label: Expanded(
-                  child: Row(
-                    children: [
-                      Text(
-                        S.of(context).condition.toUpperCase(),
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodySmall!
-                            .copyWith(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w300,
-                            color: Palette.current.white),
-                      ),
-                      GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _ascendingDate = false;
-                              _ascendingPrice = false;
-                              if (_ascendingCondition) {
-                                histories.sort((a, b) =>
-                                    b.priority.compareTo(a.priority));
-                              } else {
-                                histories.sort((a, b) =>
-                                    a.priority.compareTo(b.priority));
-                              }
-                              if (!_ascendingCondition) {
-                                _ascendingCondition = true;
-                              } else {
-                                _ascendingCondition = false;
-                              }
-                            });
-                          },
-                          child: Icon(
+                label: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _ascendingDate = false;
+                        _ascendingPrice = false;
+                        if (_ascendingCondition) {
+                          histories
+                              .sort((a, b) => b.priority.compareTo(a.priority));
+                        } else {
+                          histories
+                              .sort((a, b) => a.priority.compareTo(b.priority));
+                        }
+                        if (!_ascendingCondition) {
+                          _ascendingCondition = true;
+                        } else {
+                          _ascendingCondition = false;
+                        }
+                      });
+                    },
+                    child: Expanded(
+                      child: Row(
+                        children: [
+                          Text(
+                            S.of(context).condition.toUpperCase(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w300,
+                                    color: Palette.current.white),
+                          ),
+                          Icon(
                               _ascendingCondition
                                   ? Icons.expand_more
                                   : Icons.expand_less,
-                              size: 20))
-                    ],
-                  ),
-                ),
+                              size: 20)
+                        ],
+                      ),
+                    )),
               ),
               DataColumn(
-                label: Expanded(
-                  child: Row(
-                    children: [
-                      Text(
-                        S.of(context).price,
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodySmall!
-                            .copyWith(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w300,
-                            color: Palette.current.white),
-                      ),
-                      GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _ascendingDate = false;
-                              _ascendingCondition = false;
-                              if (_ascendingPrice) {
-                                histories.sort((a, b) => a
-                                    .productItemPrice
-                                    .compareTo(b.productItemPrice));
-                              } else {
-                                histories.sort((a, b) => b
-                                    .productItemPrice
-                                    .compareTo(a.productItemPrice));
-                              }
-                              if (!_ascendingPrice) {
-                                _ascendingPrice = true;
-                              } else {
-                                _ascendingPrice = false;
-                              }
-                            });
-                          },
-                          child: Icon(
+                label: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _ascendingDate = false;
+                        _ascendingCondition = false;
+                        if (_ascendingPrice) {
+                          histories.sort((a, b) =>
+                              a.productItemPrice.compareTo(b.productItemPrice));
+                        } else {
+                          histories.sort((a, b) =>
+                              b.productItemPrice.compareTo(a.productItemPrice));
+                        }
+                        if (!_ascendingPrice) {
+                          _ascendingPrice = true;
+                        } else {
+                          _ascendingPrice = false;
+                        }
+                      });
+                    },
+                    child: Expanded(
+                      child: Row(
+                        children: [
+                          Text(
+                            S.of(context).price,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w300,
+                                    color: Palette.current.white),
+                          ),
+                          Icon(
                               _ascendingPrice
                                   ? Icons.expand_more
                                   : Icons.expand_less,
-                              size: 20)),
-                    ],
-                  ),
-                ),
+                              size: 20),
+                        ],
+                      ),
+                    )),
               ),
             ],
             rows: histories
